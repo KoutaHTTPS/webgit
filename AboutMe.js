82bb4230b677afbcb8bd889496129ddf3db2ed92
Leonel <leonelvillalba75@hotmail.com>
2022-11-10 13:27:11 -0300

    cambios

---
+++ AboutMe.js
@@ -0,0 +1,10 @@
+/*let edad = prompt ("Decime tu edad: ")
+
+
+if ( edad <= 16) {
+    alert("No podes ingresar, edad menor a la necesaria");
+}*/
+
+let edad = 30;
+let nombre = "Leonel";
+let tienemascota = true;
